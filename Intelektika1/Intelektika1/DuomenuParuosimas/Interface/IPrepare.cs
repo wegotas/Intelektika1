838a6169119494e701feda21adc1ae8ed0343595
Rolandas Černiauskas <dekstop1@gmail.com>
2017-01-31 09:43:29 +0200

    Mano dalis excelio nuskaitymas

---
+++ Intelektika1/Intelektika1/DuomenuParuosimas/Interface/IPrepare.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intelektika1.DuomenuParuosimas.Interface
+{
+    interface IPrepare
+    {
+        List<Klases.PlanuProperty> GetAllProperties(string keliasIkiExcelio);
+        List<Klases.Reiksmes> Uzpildymas(string keliasIkiExcelio, List<Klases.PlanuProperty> Properties);
+    }
+}
