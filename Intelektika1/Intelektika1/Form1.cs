a080494105ded6512b06a3d1644bf4f998ba2b9d
Rolandas Černiauskas <dekstop1@gmail.com>
2017-02-01 09:38:05 +0200

    Changes

--- Intelektika1/Intelektika1/Form1.cs
+++ Intelektika1/Intelektika1/Form1.cs
@@ -68,11 +68,11 @@ namespace Intelektika1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            DuomenuParuosimas.Klases.Generate Generate = new DuomenuParuosimas.Klases.Generate();
+            /*DuomenuParuosimas.Klases.Generate Generate = new DuomenuParuosimas.Klases.Generate();
             var Properciai = Generate.GetAllProperties(@"C:\Users\Rolandas\Desktop\Planai-su-diagrama.xlsx"); //Savo patha reiks nurodyti(galima bus padaryt kad pasirenki excelio faila) 
             var Uzpildytas = Generate.Uzpildymas(@"C:\Users\Rolandas\Desktop\Planai-su-diagrama.xlsx", Properciai);
             MessageBox.Show("Čia ne messageboxas kurio tu ieškai");
-            MessageBox.Show("WTF kas per gitas");
+            MessageBox.Show("WTF kas per gitas");*/
         }
     }
 }
